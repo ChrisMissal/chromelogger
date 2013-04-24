a5915c6d550ce24ebb2b97c3ee46d1015901d594
Chris Missal <chris.missal@gmail.com>
2013-04-24 07:37:18 -0500

    one day i will learn how versions work, derp

--- src/ChromeLogger/Properties/AssemblyInfo.cs
+++ src/ChromeLogger/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b9d5103b-0d4c-45c2-8421-adc4e687f0b4")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.1.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
